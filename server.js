15f42596d6b6d77b569be668d6613a278e375c2f
Feduyaz <dagny2631@gmail.com>
2025-03-17 23:26:06 -0400

    l

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const app = express();
 const port = process.env.PORT || 10000;
 
 // Serve static files from the current directory
-app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Serve HTML files for the defined routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'about.html')));
+
 app.get('/projects', (req, res) => res.sendFile(path.join(__dirname, 'projects.html')));
+
 app.get('/capstone', (req, res) => res.sendFile(path.join(__dirname, 'capstone.html')));
+
 app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'contact.html')));
+
 
 // Start the server
 app.listen(port, () => {
