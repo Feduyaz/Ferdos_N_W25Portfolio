50647b1d574c3b9e809a26ff35c620ddd41ee4c0
Feduyaz <dagny2631@gmail.com>
2025-03-17 23:12:46 -0400

    f commit

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the same directory as HTML files
 app.use(express.static(__dirname));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Define routes for each HTML file
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/project', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project.html'));
+});
+
+app.get('/bio', (req, res) => {
+    res.sendFile(path.join(__dirname, 'bio.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+app.get('/capstone', (req, res) => {
+    res.sendFile(path.join(__dirname, 'capstone.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
