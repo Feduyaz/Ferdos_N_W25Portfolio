376ca9226ec3037d88ca2af67bc55fa2300256c0
Feduyaz <dagny2631@gmail.com>
2025-03-17 23:22:37 -0400

    last commit

--- server.js
+++ server.js
@@ -1,39 +1,21 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
-// Serve static files from the same directory as HTML files
-app.use(express.static(__dirname));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname, '')));
 
-// Define routes for each HTML file
+// Serve HTML files for the defined routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.get('/project', (req, res) => {
-    res.sendFile(path.join(__dirname, 'project.html'));
-});
-
-app.get('/bio', (req, res) => {
-    res.sendFile(path.join(__dirname, 'bio.html'));
-});
-
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'contact.html'));
-});
-
-app.get('/capstone', (req, res) => {
-    res.sendFile(path.join(__dirname, 'capstone.html'));
-});
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'about.html'));
-});
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'about.html')));
+app.get('/projects', (req, res) => res.sendFile(path.join(__dirname, 'projects.html')));
+app.get('/capstone', (req, res) => res.sendFile(path.join(__dirname, 'capstone.html')));
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'contact.html')));
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
